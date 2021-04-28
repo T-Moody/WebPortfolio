98491d741dc2b8e256f80a4ed132ee5ad7ef87b0
T-Moody <69010221+T-Moody@users.noreply.github.com>
2021-04-27 22:51:35 -0400

    - Add a script to open default email. - User can now send message. - Change email to correct email in contact section.

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,5 +1,18 @@
-function sendEmail(name, email, subject, message) {
-    Host: "smtp.gmail.com",
-    Username: "tyler42294@gmail.com",
-    Password: ""
+function sendEmail()
+{
+    var args = [];
+    if (typeof subject !== 'undefined') {
+        args.push('subject=' + encodeURIComponent(document.getElementById("subject").value));
+    }
+    if (typeof body == 'undefined') {
+        args.push('body=' + encodeURIComponent(document.getElementById("message").value 
+        + "\n\nThank you, \n" + document.getElementById("name").value) + "\n");
+    }
+
+    var url = 'mailto:' + encodeURIComponent("tylermoody@delta.edu");
+    if (args.length > 0) {
+        url += '?' + args.join('&');
+    }
+
+    window.location.href = url;
 }
